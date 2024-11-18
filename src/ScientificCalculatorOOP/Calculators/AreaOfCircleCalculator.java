a04ad6bcb346cfc1d1aad60cbf5ac15c1c0416f5
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-11-18 10:15:34 +0400

    Added a calculator for Area of circle, might make it modular in next commits.

--- src/ScientificCalculatorOOP/Calculators/AreaOfCircleCalculator.java
+++ src/ScientificCalculatorOOP/Calculators/AreaOfCircleCalculator.java
@@ -1,4 +1,30 @@
 package ScientificCalculatorOOP.Calculators;
 
-public class AreaOfCircleCalculator {
+import ScientificCalculatorOOP.DataClasses.BaseData;
+import ScientificCalculatorOOP.DataClasses.Circle;
+import ScientificCalculatorOOP.Interfaces.CalculatorInterface;
+
+import java.util.Scanner;
+
+public class AreaOfCircleCalculator implements CalculatorInterface {
+    static Scanner scanner = new Scanner(System.in);
+    static BaseData baseData = new BaseData();
+
+    public static Circle insertCircleData() {
+        Circle circle = new Circle();
+        System.out.print("Enter radius: ");
+        circle.setRadius(scanner.nextDouble());
+
+        baseData.setUnit("m^2");
+        circle.setBaseData(baseData);
+
+        return circle;
+    }
+
+    @Override
+    public void calculate() {
+        Circle circleData = insertCircleData();
+        Double areaOfCircle = Math.PI * Math.pow(circleData.getRadius(), 2);
+        System.out.println("Area of Circle = " + areaOfCircle + circleData.getBaseData().getUnit());
+    }
 }
