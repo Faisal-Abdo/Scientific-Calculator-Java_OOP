7c97ab4d1df187ad11cd2a663bef88348dd175a2
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-11-18 09:58:12 +0400

    Added a calculator for Simple Interest and Compound Interest.

--- src/ScientificCalculatorOOP/Calculators/SimpleInterestCalculator.java
+++ src/ScientificCalculatorOOP/Calculators/SimpleInterestCalculator.java
@@ -1,4 +1,40 @@
 package ScientificCalculatorOOP.Calculators;
 
-public class SimpleInterestCalculator {
+import ScientificCalculatorOOP.DataClasses.Interest;
+import ScientificCalculatorOOP.Interfaces.CalculatorInterface;
+
+import java.util.Scanner;
+
+public class SimpleInterestCalculator implements CalculatorInterface {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static Interest insertInterestData() {
+        Interest interest = new Interest();
+        System.out.print("Enter Principal: ");
+        Double principal = scanner.nextDouble();
+        interest.setPrincipal(principal);
+        System.out.print("Enter Rate: ");
+        Double rate = scanner.nextDouble();
+        interest.setRate(rate);
+        System.out.print("Enter Time: ");
+        Double time = scanner.nextDouble();
+        interest.setTime(time);
+
+        //No unit needed here
+
+        return interest;
+    }
+
+    public static void calculateCompoundInterest() {
+        Interest interestData = insertInterestData();
+        Double compoundInterest = interestData.getPrincipal() * Math.pow((1 + interestData.getRate() / 100), interestData.getTime()) - interestData.getPrincipal();
+        System.out.println("Compound Interest = " + compoundInterest);
+    }
+
+    @Override
+    public void calculate() {
+        Interest interestData = insertInterestData();
+        Double simpleInterest = (interestData.getPrincipal() * interestData.getRate() * interestData.getTime()) / 100;
+        System.out.println("Simple Interest = " + simpleInterest);
+    }
 }
