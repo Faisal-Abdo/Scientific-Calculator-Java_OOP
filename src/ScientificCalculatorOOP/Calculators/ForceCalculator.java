16c7560de6f04849db4682470427371a637efbdd
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2024-11-18 09:07:31 +0400

    Added a calculator for Force + illustrating its composition with Motion.

--- src/ScientificCalculatorOOP/Calculators/ForceCalculator.java
+++ src/ScientificCalculatorOOP/Calculators/ForceCalculator.java
@@ -1,4 +1,35 @@
 package ScientificCalculatorOOP.Calculators;
 
-public class ForceCalculator {
+import ScientificCalculatorOOP.DataClasses.Motion;
+import ScientificCalculatorOOP.DataClasses.Physics;
+import ScientificCalculatorOOP.Interfaces.CalculatorInterface;
+
+import java.util.Scanner;
+
+public class ForceCalculator implements CalculatorInterface {
+    static Scanner scanner = new Scanner(System.in);
+    static Motion motion = new Motion();
+
+    public static Physics insertDataForForce() {
+        Physics physics = new Physics();
+
+        System.out.print("Enter Mass: ");
+        Double mass = scanner.nextDouble();
+        physics.setMass(mass);
+        System.out.print("Enter Acceleration: ");
+        Double acceleration = scanner.nextDouble();
+        motion.setAcceleration(acceleration);
+
+        motion.setUnit("N");
+        physics.setMotionData(motion); //notice use of composition
+
+        return physics;
+    }
+
+    @Override
+    public void calculate() {
+        Physics physicsData = insertDataForForce();
+        Double force = physicsData.getMass() * physicsData.getMotionData().getAcceleration();   //access Motion object first within Physics then acceleration that is stored in Motion
+        System.out.println("Force = " + force + physicsData.getMotionData().getUnit());
+    }
 }
